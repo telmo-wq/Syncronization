97577de9ed823eac094fb88dbcf2753d4a4024cc
telmo-wq <telmomcalheiros@gmail.com>
2026-05-10 18:38:13 -0300

    Esboço de funções

--- src/sync_car.c
+++ src/sync_car.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 
 
-int main(){
-    printf("Primeiro teste\n");
+
+typedef struct Station {
+    int passageiros;
+    int assentos_livres;
+    int passageiros_embarcados;
+    pthread_mutex_t station_mutex;
+    pthread_cond_t esperar_carro;  //condition para um passageiro esperar o próximo assento
+    pthread_cond_t confirmar_saida;  //condition para um carro esperar todos os passageiros estarem prontos
+} Station;
+
+
+void station_init(Station *station){
+    station->passageiros = 0;
+    station->assentos_livres = 50;
+    station->passageiros_embarcados = 0;
+
+    pthread_mutex_init(&station->station_mutex, NULL);
+    pthread_cond_init(&station->esperar_carro, NULL);
+    pthread_cond_init(&station->confirmar_saida, NULL);
+
+}
+
+void station_wait_for_car(Station *station){
+    pthread_mutex_lock(&station->station_mutex);
+    station->passageiros++;
+
+    while(station->assentos_livres == 0){
+        pthread_cond_wait(&station->esperar_carro, &station->station_mutex);
+    }
+
+    station->assentos_livres--;
+    pthread_mutex_unlock(&station->station_mutex);
 }
